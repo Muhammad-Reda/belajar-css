b23ee45c02ef1e6d5b790ae35a536e7a9e31cb6a
muhammad reda <muhammadreda287@gmail.com>
2025-02-10 22:20:24 +0700

    twitter css mobile sidebar

--- twitter/twitter.js
+++ twitter/twitter.js
@@ -1,19 +1,59 @@
 let lastScroll = 0;
 const topContainer = document.querySelector(".top-container");
 const actoinContainer = document.querySelector(".action");
+const profilePicture = document.querySelector(".profile");
+const sidebarRight = document.querySelector(".sidebar-container .right");
+const sidebarContainer = document.querySelector(".sidebar-container");
+const settingSupport = document.querySelector(".setting-support");
+const dropdownItem = document.querySelector(".sidebar-bottom-dropdown-item");
+
+let showSidebar = false;
 
 window.addEventListener("scroll", () => {
-    const currentScroll = window.scrollY;
+    if (!showSidebar) {
+        const currentScroll = window.scrollY;
 
-    if (currentScroll > lastScroll) {
-        // Scrolling down
-        topContainer.classList.add("hide-up");
-        actoinContainer.classList.add("hide-down");
-    } else {
-        // Scrolling up
-        topContainer.classList.remove("hide-up");
-        actoinContainer.classList.remove("hide-down");
+        if (currentScroll > lastScroll) {
+            topContainer.classList.add("hide-up");
+            actoinContainer.classList.add("hide-down");
+        } else {
+            topContainer.classList.remove("hide-up");
+            actoinContainer.classList.remove("hide-down");
+        }
+
+        lastScroll = currentScroll;
     }
+});
 
-    lastScroll = currentScroll;
+profilePicture.addEventListener("click", () => {
+    showSidebar = !showSidebar;
+    if (showSidebar) {
+        document.body.style.overflow = "hidden";
+        sidebarContainer.classList.add("show-sidebar");
+
+        // Tunggu sidebar selesai transisi baru tampilkan overlay
+        setTimeout(() => {
+            sidebarRight.style.opacity = "1";
+        }, 200); // 300ms sesuai dengan durasi transisi sidebar
+    } else {
+        document.body.style.overflow = "";
+        sidebarRight.style.opacity = "0";
+        sidebarContainer.classList.remove("show-sidebar");
+    }
+});
+
+sidebarRight.addEventListener("click", () => {
+    showSidebar = false;
+    document.body.style.overflow = "";
+    sidebarRight.style.opacity = "0"; // Langsung sembunyikan overlay
+
+    // Tunggu sedikit sebelum menghilangkan sidebar dengan animasi
+    setTimeout(() => {
+        sidebarContainer.classList.remove("show-sidebar");
+    }, 50);
+});
+
+settingSupport.addEventListener("click", () => {
+    settingSupport.classList.toggle("active");
+    dropdownItem.classList.toggle("show");
 });
